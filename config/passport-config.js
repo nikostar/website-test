a7d9a45344430c0cf72ae06b86cafb2354d14667
Alexandre <alexandreahfat@gmail.com>
2020-08-03 20:03:25 +0100

    login/register working dodgily

---
+++ config/passport-config.js
@@ -0,0 +1,31 @@
+//Script for all passport stuff
+
+const LocalStrategy=require('passport-local').Strategy
+const bcrypt=require('bcrypt')
+
+function initialize(passport,getUserByEmail){
+    const authenticateUser=async (email,password,done)=>{
+        const user = getUserByEmail(email)
+        if(user==null){
+            console.log('good1')
+            return done(null,false, {message:'No user with that email'})
+        }
+
+        try{
+            if(await bcrypt.compare(password,user.password)){
+                return done(null,user)
+            }else{
+                return done(null,false, {message:'Password incorrect'})
+            }
+        }catch(e){
+            return done(e)
+        }
+        console.log(message)
+    }
+    
+    passport.use(new LocalStrategy({usernameField: 'email'}, authenticateUser))
+    passport.serializeUser((user,done)=>{ })
+    passport.deserializeUser((id,done)=>{ })
+}
+
+module.exports=initialize
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
+//moving between pages and storing the passwords
 const express=require('express')
+const passport=require('passport')
 const router=express.Router()
 const bcrypt=require('bcrypt')
+const flash=require('express-flash')
+const session=require('express-session')
+const dotenv= require('dotenv')
+const initializePassport=require('./../config/passport-config')
+initializePassport(passport,email =>{
+    return users.find(user=>user.email===email)
+})
 const users=[]
 
+dotenv.config({path:'./../config/config.env'})
 router.use(express.urlencoded({extended:false}))
+
+router.use(flash())
+router.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+router.use(passport.initialize())
+router.use(passport.session())
 //login/landing page
 //@route Get /
 router.get('/',(req,res)=>{
+    res.locals.session = req.flash(); //.{message:'123232'};
     res.render('Login',{
         layout: 'login',
     })
@@ -19,6 +39,13 @@ router.get('/register',(req,res)=>{
         layout: 'register',
     })
 })
+
+router.post('/',passport.authenticate('local',{
+    successRedirect:'/dashboard',
+    failureRedirect: '/',
+    failureFlash: true
+
+}))
 
 router.post('/register',async (req,res)=>{
     try{
